a254afadefc3a28cb2fe993c45fd345bb7b8c58b
fijik124 <tomanfilip7@gmail.com>
2026-03-22 11:57:16 +0100

    Update next.config.mjs

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -4,6 +4,8 @@ import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 
 const json = require("./package.json");
+const sentryAuthToken = process.env.SENTRY_AUTH_TOKEN;
+const shouldUploadSentryRelease = Boolean(sentryAuthToken);
 
 /**
  * @template {import("next").NextConfig} T
@@ -67,11 +69,12 @@ export default (phase, defaultConfig) => {
           org: "snailycad",
           setCommits: true,
           project: "snailycad-client",
-          authToken: "bce2b8a2e79f4336a3b115f171e675ab639d2d1f809a40919ca4493257a9c0e0",
+          authToken: sentryAuthToken,
           release: json.version,
           silent: true,
           hideSourceMaps: false,
           widenClientFileUpload: true,
+          dryRun: !shouldUploadSentryRelease,
         },
         {
           tunnelRoute: "/api/monitoring-tunnel",
